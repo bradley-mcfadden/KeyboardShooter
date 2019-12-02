e3c3877cdc4d7c9df333516dcb30ff8d5df1fc9d
Bradley McFadden <bmcfadden00@gmail.com>
2019-12-01 22:49:29 -0500

    Finished the web scraper for medical terms

--- med_terms_scraper.py
+++ med_terms_scraper.py
@@ -3,29 +3,34 @@ import os
 import sys
 import shutil
 from bs4 import BeautifulSoup
+from time import sleep
 
 def main(argv):
     path = os.getcwd() + "\\" + "medical_dict.txt"
 
     scraper = cfscrape.create_scraper()
-    start = ord('a')
+    start = ord('v')
     while (start < ord('z') + 1):
-        print(start)
-        print("https://globalrph.com/medterm/" + chr(start))
+        sleep(10)
+        # print(start)
+        # print("https://globalrph.com/medterm/" + chr(start))
         r = scraper.get("https://globalrph.com/medterm/" + chr(start))
 
-        print("opened the dict")
+        # print("opened the dict")
 
         soup = BeautifulSoup(r.content, "html.parser")
         table = soup.find("tbody")
-        links = soup.find_all("span", { "class" : "font-color-blue" })
+        links = table.find_all("span", { "class" : "font-color-blue" })
         # from b onward the tag changes to just a strong
+        if len(links) == 0:
+            links = table.find_all("strong")
 
         with open(path, "a+") as f:
             for link in links:
-                print(link.text)
+                print(link.text.lower())
                 f.write(link.text.lower() + '\n')
         start += 1
+        
 
     
 if __name__ == "__main__":
